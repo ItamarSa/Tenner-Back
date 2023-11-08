23b19ede4d3557981dfc226c0e28c8f844bf3fae
yinonab <yinonabarjel@gmail.com>
2023-11-08 18:44:52 +0200

    F/add mongo atlas

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,9 @@
 export default {
-    dbURL: 'mongodb://127.0.0.1:27017',
+    // dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://yinonabarjel:Wishime1@clustertenner.ueloxgd.mongodb.net/',
+
     // dbURL: 'mongodb+srv://yinonabarjel:Wishime1@cluster0.pn2bkb6.mongodb.net/?retryWrites=true&w=majority',
-    dbName: 'gigDB',
+    // dbName: 'gigDB',
+    dbName: 'Tenner',
     // dbName: 'userDB',
 }
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,13 @@
+// export default {
+//     dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+//     dbName: 'tester_db',
+// }
 export default {
-    dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-    dbName: 'tester_db',
+    // dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://yinonabarjel:Wishime1@clustertenner.ueloxgd.mongodb.net/',
+
+    // dbURL: 'mongodb+srv://yinonabarjel:Wishime1@cluster0.pn2bkb6.mongodb.net/?retryWrites=true&w=majority',
+    // dbName: 'gigDB',
+    dbName: 'Tenner',
+    // dbName: 'userDB',
 }
